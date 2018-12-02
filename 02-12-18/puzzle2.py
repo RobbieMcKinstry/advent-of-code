814deb4cdd03b9864e50c01ceddb8bd437f58190
Robbie McKinstry <thesnowmancometh@gmail.com>
2018-12-02 16:31:40 -0500

    correct answer for #2

--- 02-12-18/puzzle2.py
+++ 02-12-18/puzzle2.py
@@ -1,18 +1,15 @@
 with open('input.txt', 'r') as file:
     ids = []
     for line in file:
-        ids.append(line)
+        ids.append(line.strip())
 
-    for i in range(1, len(ids)):
-        first  = ids[i-1]
-        second = ids[i]
-        difference_count = 0
-        differences = []
-        for chars in zip(first, second):
-            if chars[0] != chars[1]:
-                difference_count += 1
-                differences.append(chars[0])
-                differences.append(chars[1])
-        if difference_count <= 1:
-            print first, '\t', second
-            print differences
+    for index, first in enumerate(ids):
+        for second in ids[index+1:]:
+            difference_count = 0
+            differences = []
+            for i, chars in enumerate(zip(first, second)):
+                if chars[0] != chars[1]:
+                    difference_count += 1
+                    differences.append(i)
+            if difference_count <= 1:
+                print first[:differences[0]] + first[differences[0]+1:]
